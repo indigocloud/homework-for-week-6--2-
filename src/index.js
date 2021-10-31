5109264e8fee5fee59c3969f4dfa3472cec477dc
indigocloud <lucy.hanman@gmail.com>
2021-10-31 15:38:06 +0000

    added day data

--- src/index.js
+++ src/index.js
@@ -83,28 +83,28 @@ getForecast(response.data.coord)
 
 }
 
-function displayForecast() {
-
+function displayForecast(response) {
+let forecast = response.data.daily;
   
   let forecastElement = document.querySelector("#forecast");
 
 let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+  days.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${forecastDay.dt}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max} </span>
+          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min} </span>
         </div>
       </div>
   `;
