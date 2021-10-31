a284171aaff23ef18fa18484fc75d1464ff93396
indigocloud <lucy.hanman@gmail.com>
2021-10-31 14:47:50 +0000

    added apriUrl and get axios command

--- src/index.js
+++ src/index.js
@@ -50,6 +50,16 @@ h2.innerHTML = `${city} <div> ${day} ${month} ${date}, ${hour}:${minutes}, ${yea
 
 //Week 5 below
 
+function getForecast(coordinates) {
+
+console.log(coordinates);
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid= ${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
+
+
 function displayWeatherCondition(response) {
   let windElement = document.querySelector("#wind");
   windElement = Math.round(response.data.wind.speed);
@@ -68,9 +78,14 @@ function displayWeatherCondition(response) {
   iconELement.setAttribute("src",
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   h2.innerHTML = `${city} <div> ${day} ${month} ${date}, ${hour}:${minutes}, ${year}, ${temperature} degrees | Wind speed ${windElement}km/h`;
+
+getForecast(response.data.coord)
+
 }
 
-function displayForecast() {
+function displayForecast(response) {
+console.log(response.data);
+  
   let forecastElement = document.querySelector("#forecast");
 
 let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
