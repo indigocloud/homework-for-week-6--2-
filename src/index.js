654d4dc00ba12fd810aee465d0e6e02d4ce271c9
indigocloud <lucy.hanman@gmail.com>
2021-11-07 12:20:30 +0000

    added ;

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ getForecast(response.data.coord)
 function formatDay(timestamp) {
 let date = new date(timestamp * 1000);
 let day = date.getday();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 return days[day];
 }
